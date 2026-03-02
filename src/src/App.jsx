7835c3753e1fae5a4b0719b1beadc9234510d4cd
BoyCinemix2 <adeniranboye06@gmail.com>
2026-03-02 23:57:22 +0100

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div style={{background:'black',color:'white',minHeight:'100vh',textAlign:'center',padding:'100px'}}>
+      <h1>BOYCINEMIX</h1>
+      <p>Cinematographer • Video Editor • Storyteller • Film Director</p>
+
+      <a href="https://wa.me/2349011119716">
+        <button>Hire Me</button>
+      </a>
+
+      <p>Email: adeniranboye06@gmail.com</p>
+      <p>TikTok: historiancinemixnarrativeletvme</p>
+    </div>
+  )
+}
